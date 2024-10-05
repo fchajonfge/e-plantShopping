34e8baf5eb4144418b78ba64e881de06f1155074
Fernando Chajón <fchajon@genesisempresarial.com>
2024-10-05 10:47:58 -0600

    finish app

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,29 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalPlant: 0
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+        state.totalPlant = state.totalPlant + 1;
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
+      state.totalPlant = state.totalPlant - 1;
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
